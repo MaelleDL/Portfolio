38e9c768f355d30cc5535f20d68486f29b903e9a
MaelleDaniel <maelledaniel2@gmail.com>
2021-11-16 11:48:03 +0100

    ortho!!!

--- src/components/A-propos.js
+++ src/components/A-propos.js
@@ -32,7 +32,7 @@ const Apropos = () => {
 
                         <p>En mars 2020, après huit mois de formation chez Simplon.co, j'ai obtenu mon <span>titre professionnel de développeuse web et web mobile</span>. Je continue à <span>me former en autodidacte</span> (edx, openclassroom, codecademy... ) et à m'exercer (projets personnels, codingame) afin de m'améliorer un peu plus chaque jour.</p>
 
-                        <p>Athlète de haut niveau, je met la même énergie et adopte le même état d'esprit dans le domaine professionnel que dans mon sport: <span>détermination, dépassement de soi, résilliance, esprit d'équipe...</span> </p>  
+                        <p>Athlète de haut niveau, je met la même énergie et adopte le même état d'esprit dans le domaine professionnel que dans mon sport: <span>détermination, dépassement de soi, résilience, esprit d'équipe...</span> </p>  
                     </div>
                     <a href={CV} target="_blank" rel="noreferrer" className="button">Mon CV</a>
                 </div>
